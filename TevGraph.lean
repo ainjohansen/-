9bf0f26283ddedbfa11bc47547f840b4e9e941bb
Ghost <ainjohansen@gmail.com>
2026-09-25 17:35:46 +0900

    Import Manifestv53.lean in TevGraph.lean
    Added import for Manifestv53.lean to TevGraph.lean

--- TevGraph.lean
+++ TevGraph.lean
@@ -4,6 +4,8 @@ import Mathlib.Tactic.Ring
 import Mathlib.Tactic.NormNum
 import Mathlib.Algebra.Order.Ring.Defs
 
+import Manifestv53.lean
+
 set_option linter.style.docString false
 set_option linter.style.whitespace false
 
