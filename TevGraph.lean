37815c70c0e9766dc82ca31fd1dd93cce5ede00f
Ghost <ainjohansen@gmail.com>
2026-09-25 17:53:20 +0900

    Remove unused import from TevGraph.lean
    Removed unused import of TevGraph.Manifestv53.

--- TevGraph.lean
+++ TevGraph.lean
@@ -4,8 +4,6 @@ import Mathlib.Tactic.Ring
 import Mathlib.Tactic.NormNum
 import Mathlib.Algebra.Order.Ring.Defs
 
-import TevGraph.Manifestv53
-
 set_option linter.style.docString false
 set_option linter.style.whitespace false
 
